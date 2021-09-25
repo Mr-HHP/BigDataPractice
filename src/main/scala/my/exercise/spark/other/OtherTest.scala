aabdb369ceae3f193dd2ffcb2386a9bc041f1855
haipeng.huang@mail.hypers.com <2313117162@qq.com>
2021-09-25 16:18:41 +0800

    新增：spark权威指南提供的一些样例数据源

--- src/main/scala/my/exercise/spark/other/OtherTest.scala
+++ src/main/scala/my/exercise/spark/other/OtherTest.scala
@@ -22,6 +22,7 @@ object OtherTest {
             .master("local[*]")
             .getOrCreate()
     import spark.implicits._
+    println("202108*".substring(0, "202108*".length - 1))
     
     val seq = Seq(
       ("a", "a", "a"),
@@ -34,18 +35,24 @@ object OtherTest {
       ("b", "b", "b123"),
       ("b", "b", "b")
     )
-    val df = seq.toDF("col_1", "col_2", "col_3")
+    val seq1 = Seq(("b", "1", "2"))
+//    val df = seq.toDF("col_1", "col_2", "col_3")
+//    val dff = df.join(seq1.toDF("col_a", "col_2", "col_3"), col("col_1") === col("col_a"))
+//    dff.show(false)
+//    dff.map(row => {
+//      (row.getAs[String](2), row.getAs[String](5))
+//    }).show(false)
     
-    df.groupBy(col("col_1"), col("col_2")).count().show(false)
+//    df.groupBy(col("col_1"), col("col_2")).count().show(false)
     
-    val pairDf = df.map(row => ((row.getAs[String]("col_1"), row.getAs[String]("col_2")), row.getAs[String]("col_3")))
+//    val pairDf = df.map(row => ((row.getAs[String]("col_1"), row.getAs[String]("col_2")), row.getAs[String]("col_3")))
     
-    df.dropDuplicates().groupBy(col("col_2"), col("col_1")).count().withColumnRenamed("count", "active_users").show(false)
+//    df.dropDuplicates().groupBy(col("col_2"), col("col_1")).count().withColumnRenamed("count", "active_users").show(false)
     
-    val ds = df.as[(String, String, String)]
+//    val ds = df.as[(String, String, String)]
 //    val dss = ds.joinWith(spark.emptyDataset[(String, String, String)], col(""))
     
-    df.groupBy(col("col_2"), col("col_1")).agg(countDistinct("col_3").alias("new_3")).show(false)
+//    df.groupBy(col("col_2"), col("col_1")).agg(countDistinct("col_3").alias("new_3")).show(false)
 //    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
 //    csvDownloadLocal(fs, "file:///D:\\BigDataProject\\MySpark\\src\\main\\t", "file:///D:\\BigDataProject\\MySpark\\src\\main\\test\\test\\test")
 //    df.write.mode(SaveMode.Overwrite).csv("file:///D:\\BigDataProject\\MySpark\\src\\main\\a\\b\\c")
--- src/main/scala/my/exercise/spark/rdd/RddDemo.scala
+++ src/main/scala/my/exercise/spark/rdd/RddDemo.scala
@@ -1,5 +1,8 @@
 package my.exercise.spark.rdd
 
+import java.util.Random
+
+import org.apache.spark.HashPartitioner
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
@@ -14,16 +17,57 @@ object RddDemo {
     val spark: SparkSession = SparkSession.builder().master("local[*]").getOrCreate()
     val myCollection: Seq[String] = "Spark The Definitive Guide : Big Data Processing Made Simple".split(" ")
     val words: RDD[String] = spark.sparkContext.parallelize(myCollection, 2)
-    println(words.distinct().count())
+    val keyword: RDD[(String, String)] = words.keyBy((word: String) => word.toLowerCase.toSeq.head.toString)
+    val chars = words.flatMap(word => word.toLowerCase.toSeq)
+    val KVcharcters = chars.map(letter => (letter, 1))
+    /*
+    keyword.mapValues((word: String) => word.toUpperCase).collect().foreach(println)
+    println()
+    keyword.flatMapValues((word: String) => word.toUpperCase).collect().foreach(println)
     
-    words.filter(s => true)
+    println()
+    keyword.lookup("s")
     
-    words.randomSplit(Array(3l))
+    val map = keyword.countByKey()
+    println(map)
+    println()
     
-    words.getStorageLevel
+    val nums = spark.sparkContext.parallelize(1 to 30, 5)
+    nums.aggregate(0)(maxFunc, addFunc)
     
-    spark.sparkContext.setCheckpointDir("")
-    words.checkpoint()
+    val depth = 3
+    nums.treeAggregate(0)(maxFunc, addFunc)
     
+//    keyword.foldByKey("0")(addFunc)
+    */
+    
+    // cogroup
+    val distinct = words.flatMap(word => word.toLowerCase.toSeq).distinct()
+    val charRDD = distinct.map(c => (c, new Random().nextDouble()))
+    val charRDD2 = distinct.map(c => (c, new Random().nextDouble()))
+    val charRDD3 = distinct.map(c => (c, new Random().nextDouble()))
+    val rdd: RDD[(Char, (Iterable[Double], Iterable[Double], Iterable[Double]))] = charRDD.cogroup(charRDD2, charRDD3)
+    //    rdd.collect().foreach(println)
+    
+    println("------------------------------")
+    // join
+    val keyedChars = distinct.map(c => (c, new Random().nextDouble()))
+    val outputPartitions = 10
+    //    KVcharcters.join(keyedChars).collect().foreach(println)
+    //    KVcharcters.join(keyedChars, outputPartitions).collect().foreach(println)
+    
+    // zip
+    words.collect().foreach(println)
+    val numRange: RDD[Int] = spark
+            .sparkContext
+            .parallelize(0 to 9, 2)
+    words.zip(numRange).collect().foreach(println)
+    
+    // 控制分区
+    keyedChars.repartitionAndSortWithinPartitions(new HashPartitioner(9))
   }
+  
+  def maxFunc(left: Int, right: Int): Int = math.max(left, right)
+  
+  def addFunc(left: Int, right: Int): Int = left + right
 }
