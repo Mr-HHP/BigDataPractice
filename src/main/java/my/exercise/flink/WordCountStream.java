7f7eebc84161bee28a0c18712aeeb874a93c27c3
haipeng.huang@mail.hypers.com <2313117162@qq.com>
2022-02-08 23:47:54 +0800

    新增：通过nc命令模拟数据流，flink流式word count案例

--- src/main/java/my/exercise/flink/WordCountStream.java
+++ src/main/java/my/exercise/flink/WordCountStream.java
@@ -1,6 +1,7 @@
 package my.exercise.flink;
 
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -18,11 +19,19 @@ public class WordCountStream {
     StreamExecutionEnvironment streamExecutionEnvironment = StreamExecutionEnvironment.createLocalEnvironment();
     streamExecutionEnvironment.setParallelism(8);
     // 从文件中读取数据
-    String inputPath = "D:\\BigDataProject\\BigDataPractice\\src\\main\\resources\\word.txt";
-    DataStream<String> stringDataStreamSource = streamExecutionEnvironment.readTextFile(inputPath);
+//    String inputPath = "D:\\BigDataProject\\BigDataPractice\\src\\main\\resources\\word.txt";
+//    DataStream<String> stringDataStream = streamExecutionEnvironment.readTextFile(inputPath);
     
+    // 用parameter tool工具从程序启动参数中提取配置项
+    ParameterTool parameterTool = ParameterTool.fromArgs(args);
+    String host = parameterTool.get("host", "localhost");
+    int port = parameterTool.getInt("port", 7777);
+  
+    // 从socket文本流读取数据
+    DataStream<String> stringDataStream = streamExecutionEnvironment.socketTextStream(host, port);
+  
     // 基于数据流进行转化计算
-    DataStream<Tuple2<String, Integer>> result = stringDataStreamSource
+    DataStream<Tuple2<String, Integer>> result = stringDataStream
       .flatMap(new WordCountBatch.MyFlatMapper())
       .keyBy(0)
       .sum(1);
