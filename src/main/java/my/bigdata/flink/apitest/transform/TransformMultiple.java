b2065cfc0baea3161cb58685ae0657555bf922c7
haipeng.huang@mail.hypers.com <2313117162@qq.com>
2022-02-18 01:05:25 +0800

    新增：flink连接两条流connect算子案例

--- src/main/java/my/bigdata/flink/apitest/transform/TransformMultiple.java
+++ src/main/java/my/bigdata/flink/apitest/transform/TransformMultiple.java
@@ -2,11 +2,16 @@ package my.bigdata.flink.apitest.transform;
 
 import my.bigdata.flink.apitest.bean.SensorReading;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 
@@ -52,6 +57,30 @@ public class TransformMultiple {
     lowStream.print("low");
 //    allStream.print("all");
     processStream.print("process");
+    
+    // 2. 合流 connect 将高温流转换成二元组类型，与低温流连接合并之后，输出状态信息
+    SingleOutputStreamOperator<Tuple2<String, Double>> warnStream = highStream.map(new MapFunction<SensorReading, Tuple2<String, Double>>() {
+      @Override
+      public Tuple2<String, Double> map(SensorReading value) throws Exception {
+        return new Tuple2<>(value.getId(), value.getTemperature());
+      }
+    });
+  
+    ConnectedStreams<Tuple2<String, Double>, SensorReading> connectedStreams = warnStream.connect(lowStream);
+    SingleOutputStreamOperator<Object> resultStream = connectedStreams.map(new CoMapFunction<Tuple2<String, Double>, SensorReading, Object>() {
+      @Override
+      public Object map1(Tuple2<String, Double> value) throws Exception {
+        return new Tuple3<String, Double, String>(value.f0, value.f1, "high temp warning");
+      }
+    
+      @Override
+      public Object map2(SensorReading value) throws Exception {
+        return new Tuple2<String, String>(value.getId(), "normal");
+      }
+    });
+  
+    // 打印输出
+    resultStream.print("result");
     // 执行
     env.execute("TransformMultiple");
   }
