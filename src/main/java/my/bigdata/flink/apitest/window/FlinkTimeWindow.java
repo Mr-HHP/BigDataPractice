0f9f32c43278d827f50d4ab63eca5343558e600f
haipeng.huang@mail.hypers.com <2313117162@qq.com>
2022-02-23 00:37:27 +0800

    新增：时间窗口全量窗口函数案例

--- src/main/java/my/bigdata/flink/apitest/window/FlinkTimeWindow.java
+++ src/main/java/my/bigdata/flink/apitest/window/FlinkTimeWindow.java
@@ -1,14 +1,22 @@
 package my.bigdata.flink.apitest.window;
 
 import my.bigdata.flink.apitest.bean.SensorReading;
+import org.apache.commons.collections.IteratorUtils;
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.IterableUtils;
+import org.datanucleus.store.rdbms.table.TableImpl;
 
 /**
  * flink time Window案例
@@ -16,7 +24,7 @@ import org.apache.flink.streaming.api.windowing.time.Time;
  * @author Mr.黄
  * @date 2022/02/21
  **/
-public class TimeWindow {
+public class FlinkTimeWindow {
   public static void main(String[] args) throws Exception {
     // 创建执行环境
     StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -32,6 +40,8 @@ public class TimeWindow {
     });
     
     // 开窗测试
+    
+    // 增量聚合函数
     SingleOutputStreamOperator<Integer> resultStream = dataStream.keyBy((KeySelector<SensorReading, String>) SensorReading::getId)
       .window(TumblingProcessingTimeWindows.of(Time.seconds(15L)))
       .aggregate(new AggregateFunction<SensorReading, Integer, Integer>() {
@@ -56,8 +66,22 @@ public class TimeWindow {
         }
       });
     
+    // 全窗口函数
+    SingleOutputStreamOperator<Tuple3<String, Long, Integer>> applyStream = dataStream.keyBy(SensorReading::getId)
+      .window(TumblingProcessingTimeWindows.of(Time.seconds(15L)))
+      .apply(new WindowFunction<SensorReading, Tuple3<String, Long, Integer>, String, TimeWindow>() {
+        @Override
+        public void apply(String s, TimeWindow window, Iterable<SensorReading> input, Collector<Tuple3<String, Long, Integer>> out) throws Exception {
+          String id = s;
+          Long windowEnd = window.getEnd();
+          Integer count = IteratorUtils.toList(input.iterator()).size();
+          out.collect(new Tuple3<>(id, windowEnd, count));
+        }
+      });
+  
     // 控制台输出
-    resultStream.print();
+//    resultStream.print();
+    applyStream.print();
   
     // 执行
     env.execute("timeWindow");
