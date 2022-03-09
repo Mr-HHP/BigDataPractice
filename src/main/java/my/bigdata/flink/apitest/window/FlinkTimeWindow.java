41004480f860c638f3281bfccae555a9df96a9d3
haipeng.huang@mail.hypers.com <2313117162@qq.com>
2022-03-09 13:46:58 +0800

    修复：log4j版本升级为7.17.2，fastjson版本升级为1.2.79

--- src/main/java/my/bigdata/flink/apitest/window/FlinkTimeWindow.java
+++ src/main/java/my/bigdata/flink/apitest/window/FlinkTimeWindow.java
@@ -4,19 +4,15 @@ import my.bigdata.flink.apitest.bean.SensorReading;
 import org.apache.commons.collections.IteratorUtils;
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
-import org.apache.flink.util.IterableUtils;
-import org.datanucleus.store.rdbms.table.TableImpl;
 
 /**
  * flink time Window案例
