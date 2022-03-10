2de0381d331c0aadd02325c949f72670a30ecc01
haipeng.huang@mail.hypers.com <2313117162@qq.com>
2022-03-11 01:17:34 +0800

    新增：flink table api通过FileSystem创建临时表案例

--- src/main/java/my/bigdata/flink/apitest/table/TableApiCommonApi.java
+++ src/main/java/my/bigdata/flink/apitest/table/TableApiCommonApi.java
@@ -2,9 +2,11 @@ package my.bigdata.flink.apitest.table;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.connector.datagen.table.DataGenConnectorOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.*;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
 
 /**
  * flink table common api案例
@@ -13,15 +15,14 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
  * @date 2022/03/10
  **/
 public class TableApiCommonApi {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
-    
+        
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
         
         // 1.1 基于老版本planner的流处理
         EnvironmentSettings oldStreamSettings = EnvironmentSettings.newInstance()
-            .useOldPlanner()
             .inStreamingMode()
             .build();
         StreamTableEnvironment oldStreamTableEnv = StreamTableEnvironment.create(env, oldStreamSettings);
@@ -29,6 +30,29 @@ public class TableApiCommonApi {
         // 1.2 基于老版本planner的批处理
         StreamTableEnvironment batchTableEnv = StreamTableEnvironment.create(env, EnvironmentSettings.inBatchMode());
         
+        // 1.4 基于Blink的批处理
+        StreamTableEnvironment blinkBatchTableEnv = StreamTableEnvironment.create(env, EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build());
         
+        // 2. 表的创建：连接外部系统，读取数据
+        // 2.1 读取文件
+        String filePath = "file:///D:/BigDataProject/BigDataPractice/src/main/resources/sensor.txt";
+        TableDescriptor tableDescriptor = TableDescriptor.forConnector("filesystem")
+            .format(FormatDescriptor.forFormat("csv").build())
+            .schema(Schema.newBuilder()
+                .column("id", DataTypes.STRING())
+                .column("timestamp", DataTypes.BIGINT())
+                .column("temp", DataTypes.DOUBLE())
+                .build()
+            )
+            .option("path", filePath)
+//            .option(DataGenConnectorOptions.ROWS_PER_SECOND, 100L)
+            .build();
+        tableEnv.createTemporaryTable("inputTable", tableDescriptor);
+    
+        Table inputTable = tableEnv.from("inputTable");
+        tableEnv.toDataStream(inputTable, Row.class).print();
+        inputTable.printSchema();
+        
+        env.execute("TableApiCommonApi");
     }
 }
